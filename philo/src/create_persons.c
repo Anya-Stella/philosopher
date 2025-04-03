0b105cfe94f82dc449630542fda51d64cd614d7e
Tsubasa Ishihara <tishihar@c6r4s2.42tokyo.jp>
2025-04-03 16:57:12 +0900

    init_persons直前まで

---
+++ philo/src/create_persons.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_persons.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tishihar <tishihar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/03 16:40:32 by tishihar          #+#    #+#             */
+/*   Updated: 2025/04/03 16:55:24 by tishihar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+t_person	*create_init_persons(t_info	*info)
+{
+	t_person	*persons;
+
+	persons = (t_person *)ft_calloc(info->config->num_philo, sizeof(t_person));
+	if (persons)
+		return (NULL);
+	init_persons(persons, info);
+	return (persons);
+}
+
+static	void	init_persons(t_person *persons, t_info *info)
+{
+	
+}
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: tishihar <tishihar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 15:45:47 by tishihar          #+#    #+#             */
-/*   Updated: 2025/04/03 16:21:41 by tishihar         ###   ########.fr       */
+/*   Updated: 2025/04/03 16:55:17 by tishihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,22 +75,26 @@ int check_and_print_info(t_info *info)
 
 int	main(int ac, char **av)
 {
-	t_info	info;
+	t_info		info;
+	t_person	*persons;
 
-	// 1:コマンドライン引数をconfigにぶちこむ
-	if (set_info(ac, av, &info))
+	if (set_info(ac, av, &info) == FAILURE)
 		return (1);
+	persons = create_init_persons(&info);
 	
 	
-	// 2:人数ぶんのスレッドと、共用リソースを用意して環境を整える(ここはとりあえず人数ぶんスレッドを用意。フォークはこのスコープ変数になるわけだ)
-	check_and_print_info(&info);
 	
-	
-
-
-
-
 	// 3:mutexを使って競合をなくす
 
+
+	// test
+	check_and_print_info(&info);
+
+	// free
+	free(info.config);
+	free(info.forks);
+	// persons
 	return (0);
 }
+
+
