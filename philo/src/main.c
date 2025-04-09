dee08ffd25118ed03fff9ff1b9dfd3b4ef2360dd
Tsubasa Ishihara <tishihar@c6r2s5.42tokyo.jp>
2025-04-09 16:08:07 +0900

    philo_launchの準備

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: tishihar <tishihar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 15:45:47 by tishihar          #+#    #+#             */
-/*   Updated: 2025/04/09 15:39:20 by tishihar         ###   ########.fr       */
+/*   Updated: 2025/04/09 16:03:34 by tishihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,18 +26,13 @@ int	main(int ac, char **av)
 		return (1);
 	}
 	set_persons_to_info(&info, persons);
-	
-	// スレッドをpersonsにたいしてつくってつまりrunする
-	// philo_launch();
-	
-	
-	
-	
+	philo_launch(persons);
+
 	// test
 	// dump_info(&info);
 
-	
 	// destroy(info, persons)
+	// destroy_info(&info);
 	return (0);
 }
 
