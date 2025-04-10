a3bb568608fa4ee0bd571ebf6e797c909013f4a4
Tsubasa Ishihara <tishihar@c6r2s5.42tokyo.jp>
2025-04-10 16:19:21 +0900

    親がわのリソース開放まで。あとは子スレッドがわだけ

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: tishihar <tishihar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 15:45:47 by tishihar          #+#    #+#             */
-/*   Updated: 2025/04/09 16:03:34 by tishihar         ###   ########.fr       */
+/*   Updated: 2025/04/10 16:17:19 by tishihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,13 @@ int	main(int ac, char **av)
 		return (1);
 	}
 	set_persons_to_info(&info, persons);
-	philo_launch(persons);
+	philo_launch(persons, &info);
 
 	// test
 	// dump_info(&info);
 
-	// destroy(info, persons)
-	// destroy_info(&info);
+	free(persons);
+	destroy_info(&info);
 	return (0);
 }
 
