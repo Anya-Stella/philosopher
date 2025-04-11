4a3fce5ce80a3ce6e960938eb234aa0ca6c8e8ce
Tsubasa Ishihara <tishihar@c3r3s6.42tokyo.jp>
2025-04-11 18:24:55 +0900

    make print.c

--- philo/src/print.c
+++ philo/src/print.c
@@ -6,7 +6,7 @@
 /*   By: tishihar <tishihar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 15:02:42 by tishihar          #+#    #+#             */
-/*   Updated: 2025/04/11 17:48:48 by tishihar         ###   ########.fr       */
+/*   Updated: 2025/04/11 18:24:19 by tishihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,3 +18,32 @@ void	print_dead(t_info	*info, int	id, long time_stamp)
 	printf("%ld %i died\n", time_stamp, id);
 	// pthread_mutex_unlock(&info->print_mutex);
 }
+
+void	print_taken_fork(t_info	*info, int	id, long time_stamp)
+{
+	pthread_mutex_lock(&info->print_mutex);
+	printf("%ld %i taken a fork\n", time_stamp, id);
+	pthread_mutex_unlock(&info->print_mutex);
+}
+
+void	print_eat(t_info	*info, int	id, long time_stamp)
+{
+	pthread_mutex_lock(&info->print_mutex);
+	printf("%ld %i is eatingk\n", time_stamp, id);
+	pthread_mutex_unlock(&info->print_mutex);
+}
+
+void	print_sleep(t_info	*info, int	id, long time_stamp)
+{
+	pthread_mutex_lock(&info->print_mutex);
+	printf("%ld %i is sleeping\n", time_stamp, id);
+	pthread_mutex_unlock(&info->print_mutex);
+}
+
+void	print_think(t_info	*info, int	id, long time_stamp)
+{
+	pthread_mutex_lock(&info->print_mutex);
+	printf("%ld %i is thinking\n", time_stamp, id);
+	pthread_mutex_unlock(&info->print_mutex);
+}
+
