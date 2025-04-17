8121f17403a9091783632a7dcb51f4c8852747c1
Tsubasa Ishihara <tishihar@c3r4s6.42tokyo.jp>
2025-04-17 19:33:11 +0900

    fine_tune

--- philo/src/routine_philo.c
+++ philo/src/routine_philo.c
@@ -6,7 +6,7 @@
 /*   By: tishihar <tishihar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 16:26:13 by tishihar          #+#    #+#             */
-/*   Updated: 2025/04/17 17:13:43 by tishihar         ###   ########.fr       */
+/*   Updated: 2025/04/17 19:32:54 by tishihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,7 @@ static	void	take_fork(t_person *person, int id)
 	start_ms = person->info->start_ms;
 	if (id % 2 == 1)
 	{
-		usleep((person->info->cfg.time_to_eat * 1000) / 2);
+		usleep(500);
 		pthread_mutex_lock(person->l_fork);
 		print_taken_fork(person->info, id, get_time_stamp(start_ms));
 		pthread_mutex_lock(person->r_fork);
@@ -91,14 +91,8 @@ static	void	eat_meal(t_person *person, int id)
 	pthread_mutex_lock(&person->eat_mutex);
 	person->last_eat_time = get_current_time();
 	pthread_mutex_unlock(&person->eat_mutex);
-
-
-
 	print_eat(person->info, id, get_time_stamp(person->info->start_ms));
 	usleep(person->info->cfg.time_to_eat * 1000);
-
-
-	
 	pthread_mutex_lock(&person->eat_mutex);
 	person->eat_count++;
 	pthread_mutex_unlock(&person->eat_mutex);
