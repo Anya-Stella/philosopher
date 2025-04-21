eb2a7d6a1ca02456a639370b03631ff00ea0bf38
Tsubasa Ishihara <tishihar@c6r2s5.42tokyo.jp>
2025-04-21 18:02:04 +0900

    fix: lonely print

--- philo/src/lonely_philosopher.c
+++ philo/src/lonely_philosopher.c
@@ -6,7 +6,7 @@
 /*   By: tishihar <tishihar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 17:12:35 by tishihar          #+#    #+#             */
-/*   Updated: 2025/04/21 17:42:33 by tishihar         ###   ########.fr       */
+/*   Updated: 2025/04/21 18:01:21 by tishihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 void	how_to_die_a_lonely_philosopher(t_person *person)
 {
+	pthread_mutex_lock(&person->info->print_mutex);
 	printf("%d %i has taken a fork\n", 0, person->id);
+	pthread_mutex_unlock(&person->info->print_mutex);
 }
